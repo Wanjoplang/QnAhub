81fff6cf987f9e9e3c445e58cb1c1ecf5e9e90c3
Wanjoplang <wwansai@gmail.com>
2022-09-15 00:21:07 +0530

    Updated project files

--- islands/Books.tsx
+++ islands/Books.tsx
@@ -3,14 +3,13 @@ export default function Books() {
   return (
     <div>
       <div className="font-semibold my-4">Books</div>
-      <button className="flex gap-2 flex-wrap">
-        <div className="p-2 border-2 rounded-md" onClick={(e)=>{
+      <div className="flex gap-2 flex-wrap">
+        <button className="p-2 border-2 rounded-md" onClick={(e)=>{
           window.open("/Topics","_self");
         }}>
           <div className="text-lg">Mathematics for Class 7</div>
-          <small>R S Aggarwal, MSc, PhD</small>
-        </div>
-      </button>
+        </button>
+      </div>
     </div>
   );
 }
--- routes/Books/Mathematics_for_Class_7/Probability.tsx
+++ routes/Books/Mathematics_for_Class_7/Probability.tsx
@@ -47,6 +47,55 @@ export default function Contents() {
           <div><i>Throwing a dice</i><br/>A dice is a solid cube in shape and it has 6 faces. Eace face is a square in shape. Each face are marked with single number starting from 1 till 6. So, if we throw the dice and let it fall freely on the ground, then the upper face is the outcome which will show either 1 or 2 or 3 or 4 or 5 or 6.</div>
           <div>Thus, all possible outcomes of this experiment are <span className="font-semibold">&#123;1, 2, 3, 4, 5, 6&#125;</span></div>
         </div>
+        <br/>
+        <div className="font-semibold">Empirical Probability</div>
+        <div className="my-3">If we make <i>n</i> trials of an experiment, then, the probability of occurrence of an event <i>E</i> is defined as:</div>
+        <div className="m-3 flex gap-2 items-center">
+          <i>P(E)</i><span>=</span>
+          <div className="flex flex-col text-center">
+            <div>number of trials in which <i>E</i> occurs</div>
+            <span>---------------------------------------</span>
+            <div>total number of trials</div>
+          </div>
+        </div>
+        <div className="underline">Let us try some examples to understand this Empirical formula better</div>
+        <div className="my-2">
+          <div>
+            <div className="font-semibold">Example 1</div>
+            <div>A coin tossed 100 times and head is obtained 59 times. On tossing a coin at random, find the probability of getting</div>
+            <div className="m-2 flex gap-2">
+              <i>(i) a head,</i>
+              <i>(ii) a tail,</i>
+            </div>
+          </div>
+          <div>
+            <small><b><i>Solution</i></b></small>
+            <div>Total number of trials = 100</div>
+            <div>Number of heads obtained = 59</div>
+            <div>Number of tails obtained = (Total number of trials) - (Number of heades obtained)</div>
+            <div>Number of tails obtained = 100 - 59 = 41.</div>
+            <div className="flex gap-2 items-center">
+              <span>&#8756;</span>
+              <div className="m-3 flex gap-2 items-center">
+                <i>(i)</i>
+                <i>P(getting a head)</i><span>=</span>
+                <div>(Number of heads obtained)/(Total number of trials)</div>
+                <span>=</span>
+                <div>59/100</div>
+              </div>
+            </div>
+            <div className="flex gap-2 items-center">
+              <span></span>
+              <div className="m-3 flex gap-2 items-center">
+                <i>(ii)</i>
+                <i>P(getting a tail)</i><span>=</span>
+                <div>(Number of tails obtained)/(Total number of trials)</div>
+                <span>=</span>
+                <div>41/100</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
